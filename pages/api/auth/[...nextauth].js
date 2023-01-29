5889bd8cc24c949eba1701eb58c5b8a8cc8a5c70
BaroDev <94057083+BaoCode0202@users.noreply.github.com>
2023-01-30 01:17:24 +0700

    feat: Create Components SignIn

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,20 @@
 import NextAuth from "next-auth";
-import TwitterProvider from "next-auth/providers/twitter";
-import AppleProvider from "next-auth/providers/apple";
+// import TwitterProvider from "next-auth/providers/twitter";
+// import AppleProvider from "next-auth/providers/apple";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
-import Auth0Provider from "next-auth/providers/auth0";
+// import Auth0Provider from "next-auth/providers/auth0";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "./lib/mongodb";
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     // OAuth authentication providers...
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
+    // TwitterProvider({
+    //   clientId: process.env.TWITTER_ID,
+    //   clientSecret: process.env.TWITTER_SECRET,
+    // }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
@@ -28,11 +28,11 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
 
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
+    // Auth0Provider({
+    //   clientId: process.env.AUTH0_CLIENT_ID,
+    //   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    //   issuer: process.env.AUTH0_ISSUER,
+    // }),
   ],
   pages: {
     signIn: "/signin",
